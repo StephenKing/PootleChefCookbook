46adfb8b25bd15dbcae9098c3ac9b085db0afe86
Dominique Feyer <dfeyer@ttree.ch>
2012-09-02 22:44:08 +0200

    [TASK] Remove defaults value as they are not needed

--- recipes/ter.rb
+++ recipes/ter.rb
@@ -6,7 +6,6 @@
 #
 
 user node['pootle']['TER_l10n_user'] do
-  action :create
   system true 
   home node['pootle']['TER_l10n_homedir']
   manage_home true
@@ -15,27 +14,18 @@ end
 
 # Create home direction
 directory node['pootle']['TER_l10n_homedir'] do
-  owner "root"
-  group "root"
   mode "0755"
-  action :create
   recursive true
 end
 
 # Prepare SSH configuration
 directory node['pootle']['TER_l10n_homedir'] + "/.ssh" do
-  owner "root"
-  group "root"
   mode "0755"
-  action :create
   recursive true
 end
 
 # Deploy authorized keys
 template node['pootle']['TER_l10n_homedir'] + "/.ssh/authorized_keys" do
   source "ter.authorized_keys.txt.erb"
-  owner "root"
-  group "root"
   mode "0644"
-  action :create
 end
