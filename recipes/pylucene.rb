96660d4820f8eb5cd6fcbc586022c85da1464590
Steffen Gebert <st+github@st-g.de>
2013-06-05 18:05:39 +0200

    recipes/pylucene

--- recipes/pylucene.rb
+++ recipes/pylucene.rb
@@ -5,17 +5,25 @@
 # Copyright 2012, ttree ltd
 #
 
+# java 5 ?
 %w{sun-java5-jdk g++ python-dev subversion jcc ant make}.each do |pkg|
-  package pkg do
-    action :install
-  end
+  package pkg
 end
 
 remote_file "/usr/local/src/pylucene-2.9.4-1-src.tar.gz" do
+  # could make this an attribute
   source "http://apache.mirror.testserver.li//lucene/pylucene/pylucene-2.9.4-1-src.tar.gz"
   action :create_if_missing
+  notifies :b
 end
 
+# you could/should refactor this a bit
+# - split up into jcc and pylucene
+# - run only if there is work to do, so either add a not_if { ::File.exists?(/usr/local/src/jcc) } or use action :nothing plus a notification of the above resource
+# - use the link resource instead of `ln`
+# - use the scm subversion provider (and remove `package subversion` above)
+
+# I hope to find time the next days to give this a try
 bash "install pylucene" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
