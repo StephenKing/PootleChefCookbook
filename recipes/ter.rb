ad7e0aacca19f3cf865343d9c6b6a3662afc4e67
Steffen Gebert <st+github@st-g.de>
2013-06-05 19:11:40 +0300

    recipes/ter

--- recipes/ter.rb
+++ recipes/ter.rb
@@ -13,18 +13,21 @@ end
 
 # Create home direction
 directory node['pootle']['TER_l10n_homedir'] do
+  owner node['pootle']['TER_l10n_user']
   mode "0755"
   recursive true
 end
 
 # Prepare SSH configuration
 directory node['pootle']['TER_l10n_homedir'] + "/.ssh" do
+  node['pootle']['TER_l10n_user']
   mode "0755"
   recursive true
 end
 
 # Deploy authorized keys
 template node['pootle']['TER_l10n_homedir'] + "/.ssh/authorized_keys" do
+  owner  node['pootle']['TER_l10n_user']
   source "ter.authorized_keys.txt.erb"
   mode "0644"
 end
